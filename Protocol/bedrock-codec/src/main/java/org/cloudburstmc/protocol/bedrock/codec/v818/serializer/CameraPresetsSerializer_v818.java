dc395de41ac43de8376edeff0a7fe61a29b223b6
Minecrafterytka <k.d.aligator3@gmail.com>
2025-06-18 01:09:42 +0300

    Protocol updated

--- Protocol/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v818/serializer/CameraPresetsSerializer_v818.java
+++ Protocol/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v818/serializer/CameraPresetsSerializer_v818.java
@@ -1,25 +1,46 @@
-package org.cloudburstmc.protocol.bedrock.codec.v800.serializer;
+package org.cloudburstmc.protocol.bedrock.codec.v818.serializer;
 
 import io.netty.buffer.ByteBuf;
 import org.cloudburstmc.math.vector.Vector2f;
 import org.cloudburstmc.math.vector.Vector3f;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodecHelper;
-import org.cloudburstmc.protocol.bedrock.codec.v776.serializer.CameraPresetsSerializer_v776;
+import org.cloudburstmc.protocol.bedrock.codec.v800.serializer.CameraPresetsSerializer_v800;
 import org.cloudburstmc.protocol.bedrock.data.ControlScheme;
 import org.cloudburstmc.protocol.bedrock.data.camera.CameraAimAssistPreset;
 import org.cloudburstmc.protocol.bedrock.data.camera.CameraAudioListener;
 import org.cloudburstmc.protocol.bedrock.data.camera.CameraPreset;
 import org.cloudburstmc.protocol.common.util.OptionalBoolean;
 
-public class CameraPresetsSerializer_v800 extends CameraPresetsSerializer_v776 {
+public class CameraPresetsSerializer_v818 extends CameraPresetsSerializer_v800 {
 
-    public static final CameraPresetsSerializer_v800 INSTANCE = new CameraPresetsSerializer_v800();
-
-    protected static final ControlScheme[] VALUES = ControlScheme.values();
+    public static final CameraPresetsSerializer_v818 INSTANCE = new CameraPresetsSerializer_v818();
 
     @Override
     public void writePreset(ByteBuf buffer, BedrockCodecHelper helper, CameraPreset preset) {
-        super.writePreset(buffer, helper, preset);
+        helper.writeString(buffer, preset.getIdentifier());
+        helper.writeString(buffer, preset.getParentPreset());
+        helper.writeOptionalNull(buffer, preset.getPos(), (buf, pos) -> buf.writeFloatLE(pos.getX()));
+        helper.writeOptionalNull(buffer, preset.getPos(), (buf, pos) -> buf.writeFloatLE(pos.getY()));
+        helper.writeOptionalNull(buffer, preset.getPos(), (buf, pos) -> buf.writeFloatLE(pos.getZ()));
+        helper.writeOptionalNull(buffer, preset.getPitch(), ByteBuf::writeFloatLE);
+        helper.writeOptionalNull(buffer, preset.getYaw(), ByteBuf::writeFloatLE);
+        helper.writeOptionalNull(buffer, preset.getRotationSpeed(), ByteBuf::writeFloatLE);
+        helper.writeOptional(buffer, OptionalBoolean::isPresent, preset.getSnapToTarget(),
+                (buf, optional) -> buf.writeBoolean(optional.getAsBoolean()));
+        helper.writeOptionalNull(buffer, preset.getHorizontalRotationLimit(), helper::writeVector2f);
+        helper.writeOptionalNull(buffer, preset.getVerticalRotationLimit(), helper::writeVector2f);
+        helper.writeOptional(buffer, OptionalBoolean::isPresent, preset.getContinueTargeting(),
+                (buf, optional) -> buf.writeBoolean(optional.getAsBoolean()));
+        helper.writeOptionalNull(buffer, preset.getBlockListeningRadius(), ByteBuf::writeFloatLE);
+        helper.writeOptionalNull(buffer, preset.getViewOffset(), helper::writeVector2f);
+        helper.writeOptionalNull(buffer, preset.getEntityOffset(), helper::writeVector3f);
+        helper.writeOptionalNull(buffer, preset.getRadius(), ByteBuf::writeFloatLE);
+        helper.writeOptionalNull(buffer, preset.getMinYawLimit(), ByteBuf::writeFloatLE);
+        helper.writeOptionalNull(buffer, preset.getMaxYawLimit(), ByteBuf::writeFloatLE);
+        helper.writeOptionalNull(buffer, preset.getListener(), (buf, listener) -> buf.writeByte(listener.ordinal()));
+        helper.writeOptional(buffer, OptionalBoolean::isPresent, preset.getPlayEffect(),
+                (buf, optional) -> buf.writeBoolean(optional.getAsBoolean()));
+        helper.writeOptionalNull(buffer, preset.getAimAssistPreset(), (buf, aimAssist) -> writeCameraAimAssist(buf, helper, aimAssist));
         helper.writeOptionalNull(buffer, preset.getControlScheme(), (buf, scheme) -> buf.writeByte(scheme.ordinal()));
     }
 
@@ -49,10 +70,9 @@ public class CameraPresetsSerializer_v800 extends CameraPresetsSerializer_v776 {
 
         CameraAudioListener listener = helper.readOptional(buffer, null, buf -> CameraAudioListener.values()[buf.readUnsignedByte()]);
         OptionalBoolean effects = helper.readOptional(buffer, OptionalBoolean.empty(), buf -> OptionalBoolean.of(buf.readBoolean()));
-        OptionalBoolean alignTargetAndCameraForward = helper.readOptional(buffer, OptionalBoolean.empty(), buf -> OptionalBoolean.of(buf.readBoolean()));
         CameraAimAssistPreset aimAssist = helper.readOptional(buffer, null, buf -> readCameraAimAssist(buf, helper));
         ControlScheme controlScheme = helper.readOptional(buffer, null, buf -> VALUES[buf.readUnsignedByte()]);
 
-        return new CameraPreset(identifier, parentPreset, pos, yaw, pitch, viewOffset, radius, minYawLimit, maxYawLimit, listener, effects, rotationSpeed, snapToTarget, entityOffset, horizontalRotationLimit, verticalRotationLimit, continueTargeting, alignTargetAndCameraForward, blockListeningRadius, aimAssist, controlScheme);
+        return new CameraPreset(identifier, parentPreset, pos, yaw, pitch, viewOffset, radius, minYawLimit, maxYawLimit, listener, effects, rotationSpeed, snapToTarget, entityOffset, horizontalRotationLimit, verticalRotationLimit, continueTargeting, OptionalBoolean.empty(), blockListeningRadius, aimAssist, controlScheme);
     }
 }
